c536acafe758d33607618dd1e904239161cd79ce
Ahmed-762 <166697817+Ahmed-762@users.noreply.github.com>
2025-01-31 17:11:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -167,6 +167,6 @@ def index():
 # تشغيل البوت باستخدام Flask مع Webhook
 if __name__ == "__main__":
      # حافظ على تشغيل Flask
-    bot.remove_webhook()
+    
     bot.set_webhook(url='https://bot-download-video-k6ovk41ec-ahmed-762s-projects.vercel.app/' + API_TOKEN)
     app.run(host="0.0.0.0", port=4000)
